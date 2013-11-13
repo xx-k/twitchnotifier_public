8c42bfca192cee2fd6bf1197d963a717751f5c65
xx-k <killahxxx@gmail.com>
2013-11-13 22:49:55 +0100

    New versions released! Latest version is 0.7.7.o ! See source for changes.

--- Install.bat
+++ Install.bat
@@ -1,12 +1,12 @@
 @cls
-@if not exist twitchnotifier_0.7.7.exe goto Prompt
+@if not exist twitchnotifier_0.7.7.o.exe goto Prompt
 :Move
 @echo Moving to Startup folder...
-@cp -f twitchnotifier_0.7.7.exe "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\TwitchApplication.exe"
+@cp -f twitchnotifier_0.7.7.o.exe "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\TwitchApplication.exe"
 goto End
 :Prompt
 @echo off
-set /P filename=TwitchChecker application not found, please place app in directory and enter filename (e.g. "twitchnotifier_0.7.7.exe" without quotes): 
+set /P filename=TwitchChecker application not found, please place app in directory and enter filename (e.g. "twitchnotifier_0.7.7.o.exe" without quotes): 
 @if %filename%==quit goto End
 @if %filename%==exit goto End
 @cls
